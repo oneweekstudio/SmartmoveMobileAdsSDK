884ecad5d26caa54ff1ba71828f225dc33be8437
Hintoro <hintoro@Hintoros-MacBook-Pro.local>
2019-10-18 13:51:05 +0700

    update

--- SmartmoveMobileAdsSDK/Classes/SMADMobileAds.swift
+++ SmartmoveMobileAdsSDK/Classes/SMADMobileAds.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+@objc public protocol SMADMobileAdsProtocol {
+    
+  @objc optional  func load(_ request: SMADRequest)
+    func present(fromRootViewController rootViewController: UIViewController)
+    
+    var isReady: Bool { get }
+    var hasBeenUsed: Bool { get }
+    var responseInfo: SMADResponse? { get }
+}
+
+
+public class SMADMobileAds : SMADMobileAdsProtocol {
+    
+}
